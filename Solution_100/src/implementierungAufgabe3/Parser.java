117969dd0cea9e7bd84864a7224230cb8328593f
Johannes <johannes.struve@gmx.de>
2015-12-07 16:28:25 +0100

    - improved parser

--- Solution_100/src/implementierungAufgabe3/Parser.java
+++ Solution_100/src/implementierungAufgabe3/Parser.java
@@ -10,11 +10,14 @@ public class Parser {
 			if(t.matches("[A-Za-z]+")){
 				stack.push(new Node(t));
 			} else if(t.matches("[&|]")){
+				if(stack.elems < 1){ // less than 2 elements
+					throw new IllegalArgumentException("Found boolean operator with less than two arguments");
+				}
 				Node n2 = stack.pop();
 				Node n1 = stack.pop();
 				stack.push(new Node(t, n1, n2));
 			} else {
-				throw new IllegalArgumentException();
+				throw new IllegalArgumentException("Found invalid character");
 			}
 		}
 		
