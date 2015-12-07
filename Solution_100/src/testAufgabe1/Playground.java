62370e9721ed2eadfbc2fdbdc3a80722e7de4a31
PatrickKorus <korus@gmx.net>
2015-12-07 13:36:22 +0100

    Solution_100 is added..

---
+++ Solution_100/src/testAufgabe1/Playground.java
@@ -0,0 +1,17 @@
+package testAufgabe1;
+import main.Game;
+
+
+public final class Playground {
+
+	public Playground() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		Game g= new Game();
+		
+	}
+
+}
