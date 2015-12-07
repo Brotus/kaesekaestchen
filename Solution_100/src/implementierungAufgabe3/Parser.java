420d1ab6964f7aed785619b87295577caf1bc6ce
Johannes <johannes.struve@gmx.de>
2015-12-07 14:43:12 +0100

    - added structures

---
+++ Solution_100/src/implementierungAufgabe3/Parser.java
@@ -0,0 +1,10 @@
+package implementierungAufgabe3;
+
+public class Parser {
+	
+	public Tree parseBoolean(String s){
+		Stack stack = new Stack();
+		return stack.toTree();
+	}
+
+}
---
+++ Solution_100/src/implementierungAufgabe3/Tree.java
@@ -0,0 +1,8 @@
+package implementierungAufgabe3;
+
+public class Tree {
+	public Node root;
+	public Tree(Node root){
+		this.root = root;
+	}
+}
