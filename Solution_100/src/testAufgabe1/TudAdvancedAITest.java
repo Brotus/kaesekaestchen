4abaa082b7e34264d40566f9820be733cfa50f31
PatrickKorus <korus@gmx.net>
2015-12-07 19:15:39 +0100

    - 100 percent coverage of reference implementation

--- Solution_100/src/testAufgabe1/TudAdvancedAITest.java
+++ Solution_100/src/testAufgabe1/TudAdvancedAITest.java
@@ -9,7 +9,7 @@ import tud.game.Board;
 import tud.game.Player;
 import tug.game.ai.AdvancedAI;
 
-public class MinMaxRefrenceImplementationTest {
+public class TudAdvancedAITest {
 
 	Board basicBoard;
 	Player basicPlayer;
@@ -28,6 +28,23 @@ public class MinMaxRefrenceImplementationTest {
 	}
 	
 	@Test
+	public void adaptiveTreeDepth() {
+		Board bigBoard = new Board(5);
+		assertEquals(2, AdvancedAI.getMaxDepth(bigBoard));
+		bigBoard.nextAction(1, "basic");
+		assertEquals(3, AdvancedAI.getMaxDepth(bigBoard));
+		Board mediumBoard = new Board(3);
+		mediumBoard.nextAction(1, "basic");
+		assertEquals(4, AdvancedAI.getMaxDepth(mediumBoard));
+		Board smallBoard = new Board(2);
+		smallBoard.nextAction(1, "basic");
+		assertEquals(6, AdvancedAI.getMaxDepth(smallBoard));
+		Board square = new Board(1);
+		square.nextAction(1, "basic");
+		assertEquals(12, AdvancedAI.getMaxDepth(square));
+	}
+	
+	@Test
 	public void selectsBest() {
 		int[] edges = new int[]{1,3,4};
 		for (int e : edges) {
@@ -35,4 +52,6 @@ public class MinMaxRefrenceImplementationTest {
 		}
 		assertEquals("6",advancedAi.getNextTurn(basicBoard));
 	}
+	
+	
 }
