f3f56452929cdb51fb1f046b4944d2ef446eaae6
Johannes <johannes.struve@gmx.de>
2015-12-07 14:53:25 +0100

    - extended parser

--- Solution_100/src/implementierungAufgabe3/Parser.java
+++ Solution_100/src/implementierungAufgabe3/Parser.java
@@ -2,7 +2,17 @@ package implementierungAufgabe3;
 
 public class Parser {
 	
-	public Tree parseBoolean(String s){
+	public Tree parseBooleanEx(String s) throws IllegalArgumentException{
+		String[] sArr = s.split(" ");
+		for(String t: sArr){
+			if(t.matches("[A-Za-z]+")){
+				
+			} else if(t.matches("[&|]")){
+				
+			} else {
+				throw new IllegalArgumentException();
+			}
+		}
 		Stack stack = new Stack();
 		return stack.toTree();
 	}
