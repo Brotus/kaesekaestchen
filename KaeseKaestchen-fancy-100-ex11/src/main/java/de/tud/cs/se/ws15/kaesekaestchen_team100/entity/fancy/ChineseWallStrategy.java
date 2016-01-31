c909314d081b73c018f2a58cc574b389cd54a543
John <mtasa.lua@gmx.de>
2016-01-31 23:35:12 +0100

    - continued strategy

--- KaeseKaestchen-fancy-100-ex11/src/main/java/de/tud/cs/se/ws15/kaesekaestchen_team100/entity/fancy/ChineseWallStrategy.java
+++ KaeseKaestchen-fancy-100-ex11/src/main/java/de/tud/cs/se/ws15/kaesekaestchen_team100/entity/fancy/ChineseWallStrategy.java
@@ -1,5 +1,12 @@
 package de.tud.cs.se.ws15.kaesekaestchen_team100.entity.fancy;
 
-public abstract class ChineseWallStrategy implements FancyHandle {
+import de.tud.cs.se.ws15.kaesekaestchen_team100.entity.Map;
+
+public class ChineseWallStrategy implements FancyHandle {
+
+	@Override
+	public void action(Map gameMap) {
+		
+	}
 
 }
--- KaeseKaestchen-fancy-100-ex11/src/main/java/de/tud/cs/se/ws15/kaesekaestchen_team100/entity/fancy/FancyHandle.java
+++ KaeseKaestchen-fancy-100-ex11/src/main/java/de/tud/cs/se/ws15/kaesekaestchen_team100/entity/fancy/FancyHandle.java
@@ -1,5 +1,6 @@
 package de.tud.cs.se.ws15.kaesekaestchen_team100.entity.fancy;
 
+import de.tud.cs.se.ws15.kaesekaestchen_team100.entity.Map;
 import de.tud.cs.se.ws15.kaesekaestchen_team100.main.Game;
 
 public interface FancyHandle {
@@ -8,6 +9,6 @@ public interface FancyHandle {
 	 * NOTE: Maybe it is enough to pass the map rather than the entire game
 	 * @param game
 	 */
-	public void action(Game game);
+	public void action(Map gameMap);
 
 }
